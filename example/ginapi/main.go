dc4d0b1fdb63d8a0c1025b59ea2414601de1c5a8
supakrit.c <supakrit.c@arise.tech>
2024-04-25 10:32:28 +0700

    feat: add APIs path to validate Thai IDs

--- example/ginapi/main.go
+++ example/ginapi/main.go
@@ -4,7 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/SupTarr/intensive_go_basic_workshop/_exercises/thai_id"
 )
+
+type VerifyIdRequest struct {
+	ID string `json:"id" binding:"required"`
+}
+
+type VerifyIdResponse struct {
+	Valid bool `json:"valid"`
+}
+
+type VerifyIdErrorResponse struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	r := gin.Default()
@@ -13,5 +27,31 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.POST("/thai/ids/verify", func(c *gin.Context) {
+		var json VerifyIdRequest
+		err := c.BindJSON(&json)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, VerifyIdErrorResponse{
+				Message: err.Error(),
+			})
+
+			return
+		}
+
+		err = thai_id.Validate(json.ID)
+		if err != nil {
+			c.JSON(http.StatusOK, VerifyIdResponse{
+				Valid: false,
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, VerifyIdResponse{
+			Valid: true,
+		})
+	})
+
 	r.Run()
 }
