6445e700353fe72b1d55beb0da9286257aa7a0f1
supakrit.c <supakrit.c@arise.tech>
2024-05-03 19:18:38 +0700

    feat: add validate password

--- exercises/password/password.go
+++ exercises/password/password.go
@@ -1,5 +1,34 @@
 package password
 
+import "errors"
+
 func ValidatePassword(password string) error {
+	number := 0
+	capital := 0
+	special := 0
+
+	if len(password) < 8 {
+		return errors.New("the password must be at least 8 characters")
+	}
+
+	for _, v := range password {
+		switch {
+		case 48 <= v && v <= 57:
+			number += 1
+		case 58 <= v && v <= 64:
+			special += 1
+		case 65 <= v && v <= 90:
+			capital += 1
+		}
+	}
+
+	if number < 2 {
+		return errors.New("the password must contain at least 2 numbers")
+	} else if capital < 1 {
+		return errors.New("the password must contain at least one capital letter")
+	} else if special < 1 {
+		return errors.New("the password must contain at least one special character")
+	}
+
 	return nil
 }
